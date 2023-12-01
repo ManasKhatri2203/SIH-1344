04657cf581a59b8a6934eb8559bd5956a5cb6cd7
ManasKhatri2203 <117750332+ManasKhatri2203@users.noreply.github.com>
2023-12-01 22:34:13 +0530

    Update js.js

--- js.js
+++ js.js
@@ -8,11 +8,8 @@ document.addEventListener('DOMContentLoaded', () => {
         const file = fileInput.files[0];
 
         if (file) {
-            // You can add image processing and AI model integration here
-            // For now, display a placeholder result
             displayResults('Placeholder result: Skin condition detected.');
 
-            // Show the results container
             resultsContainer.style.display = 'block';
         }
     }
